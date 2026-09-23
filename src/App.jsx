c9c10de7865a482b1341e5306211318c6ee05785
SuryaKumar31 <srisurya285@gmail.com>
2026-09-23 18:31:53 +0530

    feat:import the FormikApp component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
+import FormikApp from "./Forms/formik/FormikApp";
+
 function App() {
   return (
     <>
-      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugiat expedita
-      dicta obcaecati labore, rem sint? Esse voluptatem quaerat quae odit nam
-      error vel praesentium porro cum, velit voluptatum necessitatibus animi.
+      <FormikApp />
     </>
   );
 }
